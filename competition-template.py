ed1e3006c90e9c30040ea288c9633f3a98b6c316
Dom Barter <dominicbarter@gmail.com>
2019-05-08 11:03:26 +0100

    Create competition-template.py

---
+++ competition-template.py
@@ -0,0 +1,24 @@
+
+import sys # import all of these!
+import vex
+from vex import *
+import math
+
+# you can make your own functions and classes used in the below three functions here.
+
+def pre_auton(): # place pre-auto code here
+    pass
+
+def autonomous(): # place autonomous code here
+    pass
+
+def drivercontrol(): # palce main control code here
+    pass
+
+
+competition = vex.Competition() # competition object
+
+competition.autonomous(autonomous) # set up callbacks for the three periods
+competition.drivercontrol(drivercontrol)
+pre_auton()
+
